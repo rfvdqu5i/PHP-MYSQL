ddfd4156ad9411cb16a0a229ecb321a5d5cf4862
Nguyễn Đức Anh <30741310+rfvdqu5i@users.noreply.github.com>
2019-03-29 16:53:30 +0700

    29

---
+++ unit07/ex02.php
@@ -0,0 +1,37 @@
+<?php
+
+class product
+{
+    var $id;
+    var $name;
+    var $amounts;
+    var $price;
+    var $origin;
+    
+    function export(){
+        echo 'Mã sản phẩm: ' . $this->id . '<br>'; 
+        echo 'Tên sản phẩm: ' . $this->name . '<br>';
+        echo 'Số lượng sản phẩm: ' . $this->amounts . '<br>';
+        echo 'Đơn giá: ' . $this->price . '<br>';
+        echo 'Xuất xứ: ' . $this->origin . '<br>';
+    }
+}
+
+$laptop = new product();
+
+$laptop->id = '001';
+$laptop->name = 'laptop';
+$laptop->amounts = '05';
+$laptop->price = '14000000';
+$laptop->origin = 'China';
+
+$iphone = new product();
+
+$iphone->id = '002';
+$iphone->name = 'iphone x';
+$iphone->amounts = '15';
+$iphone->price = '24000000';
+$iphone->origin = 'Vietnam';
+
+echo $laptop->export() . '<br>';
+echo $iphone->export() . '<br>';
